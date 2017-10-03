7f562fcd778362b73c7f8d9ca0431c25ca1e6542
kangkang <xiyoulinux.kangyijie@gmail.com>
2017-10-03 12:53:51 +0800

    update

---
+++ version2/demoClient/test.c
@@ -0,0 +1,12 @@
+/*
+ * > File Name: test.c
+ * > Author: Jack Kang
+ * > Mail: kangyijie@xiyoulinux.org
+ * > Created Time: 2017年10月03日 星期二 12时53分29秒
+ */
+
+#include <stdio.h>
+int main(void)
+{
+    printf("CI\n");
+}
