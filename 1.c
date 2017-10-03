b3679fe92651f1d9f8c5b04d5a7d491e8f6c1eab
kangkang <xiyoulinux.kangyijie@gmail.com>
2017-10-03 12:25:59 +0800

    test for CI

---
+++ 1.c
@@ -0,0 +1,12 @@
+/*
+ * > File Name: 1.c
+ * > Author: Jack Kang
+ * > Mail: kangyijie@xiyoulinux.org
+ * > Created Time: 2017年10月03日 星期二 12时25分26秒
+ */
+
+#include <stdio.h>
+int main(void)
+{
+    printf("test CI");
+}
